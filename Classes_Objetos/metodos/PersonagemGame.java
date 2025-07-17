5d5350cdec4f52af1389170cb3c47496c47583c9
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-07-17 18:01:42 -0300

    [projeto metodos] Implementacao.

--- Classes_Objetos/metodos/PersonagemGame.java
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -18,11 +18,11 @@ public class PersonagemGame {
         this.nome = nome;
     }
 
-    public void tomarDano(int valor) {
-        saudeAtual = Math.max(saudeAtual - valor, 0);
+    public void tomarDano(int quantidadeDeDano) {
+        saudeAtual = Math.max(saudeAtual - quantidadeDeDano, 0);
     }
 
-    public void receberCura(int valor) {
-        saudeAtual = Math.min(saudeAtual + valor, 100);
+    public void receberCura(int quantidadeDeCura) {
+        saudeAtual = Math.min(saudeAtual + quantidadeDeCura, 100);
     }
 }
