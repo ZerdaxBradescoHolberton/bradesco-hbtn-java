cb49aa02c913418249a06d88bad5bb2d46b50c89
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-07-17 10:45:54 -0300

    Implementação do programa de salario.

---
+++ Heranca/salario/Gerente.java
@@ -0,0 +1,11 @@
+public class Gerente extends Empregado {
+
+    public Gerente(double salarioFixo) {
+        super(salarioFixo);
+    }
+
+    @Override
+    public double calcularBonus(Departamento departamento) {
+        return departamento.alcancouMeta() ? getSalarioFixo() * 0.2 : 0;
+    }
+}
