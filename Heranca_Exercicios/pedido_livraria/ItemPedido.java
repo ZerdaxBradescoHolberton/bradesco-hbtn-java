911b0f38c3270279894e722b71d43d212e403572
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-07-17 16:40:37 -0300

    [projeto pedido_livraria] Implementacao

---
+++ Heranca_Exercicios/pedido_livraria/ItemPedido.java
@@ -0,0 +1,27 @@
+import produtos.*;
+
+public class ItemPedido {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+}
---
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Pedido {
+    private double percentualDesconto;
+    private ItemPedido[] itens;
+
+    public Pedido(double percentualDesconto, ItemPedido[] itens) {
+        this.percentualDesconto = percentualDesconto;
+        this.itens = itens;
+    }
+
+    public double getPercentualDesconto() {
+        return percentualDesconto;
+    }
+
+    public void setPercentualDesconto(double percentualDesconto) {
+        this.percentualDesconto = percentualDesconto;
+    }
+
+    public ItemPedido[] getItens() {
+        return itens;
+    }
+
+    public void setItens(ItemPedido[] itens) {
+        this.itens = itens;
+    }
+
+    public double calcularTotal() {
+        return Arrays.stream(itens).map(itemPedido -> itemPedido.getQuantidade()
+                * itemPedido.getProduto().obterPrecoLiquido()).reduce(0d, Double::sum)
+                * ((100 - percentualDesconto)/100);
+    }
+}
