e7390fe798b8381191c3be183e17b2bfdbebc1bb
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-08-05 16:46:53 -0300

    [projeto Interfaces/workflow] Implementacao.

---
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessadorVideo {
+    private final List<CanalNotificacao> canais = new ArrayList<>();
+
+    public void registrarCanal(CanalNotificacao canalNotificacao) {
+        canais.add(canalNotificacao);
+    }
+
+    public void processar(Video video) {
+        canais.stream().forEach(canalNotificacao -> {
+        });
+    }
+}
