8ddee081aba1bbe4dfdb374cc91a36261dc97537
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-08-06 06:01:36 -0300

    [projeto arquivos/0] Implementacao

--- arquivos/0/FileWritingExercise.java
+++ arquivos/0/FileWritingExercise.java
@@ -4,10 +4,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class FileWritingExercise {
+    private static List<String> fileNames = new ArrayList<>();
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
@@ -18,20 +21,26 @@ public class FileWritingExercise {
 
 
         // implemente o codigo aqui
+        if(!"sair".equals(fileName)) {
+            fileNames.add(fileName);
+            main(null);
+        }
         createFile(fileName);
-
 
 
         scanner.close();
     }
 
     private static void createFile(String fileName) throws Exception {
-        if("sair".equals(fileName))
-            System.exit(0);
-        if(!Files.exists(Path.of(fileName))) {
-            Files.createFile(Path.of(fileName));
-            System.out.println("Arquivo criado com sucesso");
-            main(null);
-        }
+        fileNames.forEach(s -> {
+            if(!Files.exists(Path.of(s))) {
+                try {
+                    Files.createFile(Path.of(s));
+                    System.out.printf("Arquivo %s criado com sucesso%n", s);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
     }
 }
