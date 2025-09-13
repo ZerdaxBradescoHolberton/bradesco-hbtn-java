ad23343ad17afa0f590a7e2626d2940f3d191294
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-13 13:22:47 -0300

    [projeto Streams_Collectors_Optional/maior_preco] Implementacao 0

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+    public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+        return pedido.getProdutos().stream().filter(produto -> produto.getCategoria()
+                .equals(CategoriaProduto.LIVRO)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+        return produtos.stream().max(Comparator.comparingDouble(Produto::getPreco)).orElse(null);
+    }
+
+}
