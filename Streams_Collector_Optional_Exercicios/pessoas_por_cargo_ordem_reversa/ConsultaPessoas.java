efd712699ff898a085b8614a4dc08fe94c824c1f
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-16 12:12:00 -0300

    [projeto Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa] Implementacao 0

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/ConsultaPessoas.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class ConsultaPessoas {
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo));
+    }
+
+    public static Map<String, List<Integer>> obterIdadePorCargo(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.mapping(Pessoa::getIdade, Collectors.toCollection(ArrayList::new))));
+    }
+
+    public static Map<String, List<Pessoa>> obterPessoasPorCargoAcimaDe40anos(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                Collectors.filtering(pessoa -> pessoa.getIdade() > 40, Collectors.toList())));
+    }
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas) {
+        return pessoas.stream().collect(Collectors.groupingBy(Pessoa::getCargo,
+                () -> new TreeMap<>(Comparator.reverseOrder()),
+                Collectors.toCollection(TreeSet::new)));
+    }
+
+
+}
