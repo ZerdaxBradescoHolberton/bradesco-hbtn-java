51ef83d4b3d3bd8967f84046c352fa457d299e92
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-16 12:28:53 -0300

    [projeto Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade] Implementacao 0

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/Pessoa.java
@@ -0,0 +1,85 @@
+import java.util.Comparator;
+import java.util.Locale;
+import java.util.Objects;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    @Override
+    public String toString() {
+        Locale.setDefault(new Locale("pt", "BR"));
+        return String.format("[%d] %s %s %d R$ %.6f", codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pessoa pessoa)) return false;
+        return codigo == pessoa.codigo
+                && idade == pessoa.idade
+                && Double.compare(salario, pessoa.salario) == 0
+                && Objects.equals(nome, pessoa.nome)
+                && Objects.equals(cargo, pessoa.cargo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(codigo, nome, cargo, idade, salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        return Comparator.comparing(Pessoa::getNome).compare(this, o);
+    }
+}
