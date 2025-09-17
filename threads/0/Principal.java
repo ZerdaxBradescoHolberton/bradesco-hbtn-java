668d9ca423ce1710b5509091d4e3a8595fb722bc
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-17 09:26:12 -0300

    [projeto threads/0] Implementacao 0

---
+++ threads/0/Principal.java
@@ -0,0 +1,32 @@
+public class Principal {
+
+
+    private static Contador contador;
+    private static Thread[] threads = new Thread[2];
+    public static void main(String[] args) {
+        contador = new Contador();
+        executaThreads();
+    }
+
+    private static void executaThreads() {
+        for(int t = 0; t < 2; t++) {
+            threads[t] = new ThreadContador(contador);
+            threads[t].start();
+        }
+
+        boolean concludedThread = false;
+        while(!concludedThread) {
+            concludedThread = true;
+            for(Thread thread : threads) {
+                if(thread.isAlive()) {
+                    concludedThread = false;
+                    break;
+                }
+            }
+        }
+        
+        System.out.printf("Valor final do contador: %d%n", contador.getContagem());
+
+    }
+
+}
