d31db92c4e6e7b29af8e36d089898a4d81740397
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-17 10:21:54 -0300

    [projeto threads/1] Implementacao 0

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,20 @@
+public class ProdutorConsumidor {
+    public static void main(String[] args) {
+        Buffer buffer = new Buffer();
+
+        // Criar e iniciar as threads do produtor e consumidor
+        Thread consumidor = new Consumidor(buffer);
+        Thread produtor = new Produtor(buffer);
+
+        consumidor.start();
+        produtor.start();
+
+        try {
+            consumidor.join();
+            produtor.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        
+    }
+}
