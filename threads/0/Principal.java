5b7d545df5f8e1d95d2e67ce540fc461f7e28ce4
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-09-17 09:55:28 -0300

    [projeto threads/0] Implementacao 2

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -1,40 +1,18 @@
 public class Principal {
 
 
-    private static Contador contador;
-    private static Thread[] threads = new Thread[2];
     public static void main(String[] args) {
-        contador = new Contador();
-        new ThreadContador(contador).start();
-        new ThreadContador(contador).start();
-        while(Thread.activeCount() > 2) {
-//            System.out.println(Thread.activeCount());
-//            System.out.println(Thread.activeCount());
-            continue;
+        Contador contador = new Contador();
+        Thread t1 = new ThreadContador(contador);
+        Thread t2 = new ThreadContador(contador);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
         System.out.printf("Valor final do contador: %d%n", contador.getContagem());
-//        executaThreads();
     }
-
-    private static void executaThreads() {
-        for(int t = 0; t < 2; t++) {
-            threads[t] = new ThreadContador(contador);
-            threads[t].start();
-        }
-
-        boolean concludedThread = false;
-        while(!concludedThread) {
-            concludedThread = true;
-            for(Thread thread : threads) {
-                if(thread.isAlive()) {
-                    concludedThread = false;
-                    break;
-                }
-            }
-        }
-
-        System.out.printf("Valor final do contador: %d%n", contador.getContagem());
-
-    }
-
 }
