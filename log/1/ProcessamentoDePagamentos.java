fa8ccc58c105cb98222f100a456a7550c444f516
Valdizar Paula de Andrade Junior <zerdax.sjc@gmail.com>
2025-10-21 07:30:00 -0300

    [Projeto log/1] - Implementação 1.0

---
+++ log/1/ProcessamentoDePagamentos.java
@@ -0,0 +1,40 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+
+import java.util.Random;
+
+
+public class ProcessamentoDePagamentos {
+
+
+    // Logger SLF4j
+    private static final Logger logger = LoggerFactory.getLogger(ProcessamentoDePagamentos.class);
+
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        for(int n = 1; n < 6; n++) {
+            Level level = Level.intToLevel((random.nextInt(3) + 2) * 10);
+            logger.info("Iniciando o processamento do pagamento {}", n);
+            switch (level) {
+                case INFO:
+                    logger.atLevel(level).log("Pagamento {} processado com sucesso.", n);
+                    break;
+                case WARN:
+                    logger.atLevel(level).log("Pagamento {} está pendente. Aguardando confirmação.", n);
+                    break;
+                case ERROR:
+                    logger.atLevel(level).log("Erro ao processar o pagamento {}: Falha na transação.", n);
+                    break;
+            }
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+}
